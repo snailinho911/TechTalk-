cd5b9f7c1e61512f284bb7a69cbe22db20b21670
snailinho911 <akov40320@gmail.com>
2025-02-19 23:17:12 +0300

    Update database.py

--- database.py
+++ database.py
@@ -5,6 +5,6 @@ import os
 
 POSTGRES_URL = os.getenv("POSTGRES_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(POSTGRES_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
